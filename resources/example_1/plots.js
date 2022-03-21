c563c645aa5c53b005d0171739a48d9712e576c2
Andrey Podvysotskiy <podvysotskiy@gmail.com>
2022-03-20 21:23:45 -0700

    stylized dashboard complete

--- resources/example_1/plots.js
+++ resources/example_1/plots.js
@@ -4,10 +4,10 @@ var trace = {
     y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
     type: "bar"
    };
-   var data = [trace];
-   var layout = {
+var data = [trace];
+var layout = {
     title: "'Bar' Chart",
     xaxis: {title: "Drinks"},
     yaxis: {title: "% of Drinks Ordered"}
    };
-   Plotly.newPlot("plotArea", data, layout);
+Plotly.newPlot("plotArea", data, layout);
